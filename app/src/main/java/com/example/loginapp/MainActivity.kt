b4cd407122e85ce8cdc0d6205a9c19969e01d196
Juan Rambal <jrambal@lya-electronic.com>
2020-09-21 14:03:51 -0500

    feat: api viewmodels

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import android.os.Bundle
 import androidx.navigation.ui.AppBarConfiguration
+import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
